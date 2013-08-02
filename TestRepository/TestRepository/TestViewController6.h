17157e4559eec7e0c2522e3cc1706207454a5a16
Chaithanya Kumar <chaithanya.kumar@robosoftin.com>
2013-08-02 11:27:09 +0530

    Committed with test view 6

---
+++ TestRepository/TestRepository/TestViewController6.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController6.h
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController6 : UIView
+
+@end
---
+++ TestRepository/TestRepository/TestViewController6.m
@@ -0,0 +1,31 @@
+//
+//  TestViewController6.m
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import "TestViewController6.h"
+
+@implementation TestViewController6
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
