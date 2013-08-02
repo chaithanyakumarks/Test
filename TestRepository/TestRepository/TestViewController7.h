3a5e2386f1adbd4ad42c83952e74740916fbe8f3
Chaithanya Kumar <chaithanya.kumar@robosoftin.com>
2013-08-02 11:28:38 +0530

    Committed with test view 7.

---
+++ TestRepository/TestRepository/TestViewController7.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController7.h
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController7 : UIView
+
+@end
---
+++ TestRepository/TestRepository/TestViewController7.m
@@ -0,0 +1,31 @@
+//
+//  TestViewController7.m
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import "TestViewController7.h"
+
+@implementation TestViewController7
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
