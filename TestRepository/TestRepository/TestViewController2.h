5e91d17f847caa23f0f6988a20300c16d82d4d13
Chaithanya Kumar <chaithanya.kumar@robosoftin.com>
2013-08-02 10:28:29 +0530

    Committed with test view controller 2

---
+++ TestRepository/TestRepository/TestViewController2.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController2.h
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController2 : UIView
+
+@end
---
+++ TestRepository/TestRepository/TestViewController2.m
@@ -0,0 +1,31 @@
+//
+//  TestViewController2.m
+//  TestRepository
+//
+//  Created by Chaithanya Kumar on 02/08/13.
+//  Copyright (c) 2013 Robosoft Technologies Pvt. Ltd. All rights reserved.
+//
+
+#import "TestViewController2.h"
+
+@implementation TestViewController2
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
